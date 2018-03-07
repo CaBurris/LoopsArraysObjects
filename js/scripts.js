10453d96f865ba144e4088683acb0dc788f2f518
Christy <caburris@ucdavis.edu>
2018-03-06 17:16:09 -0800

    debugged do...while loop

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,24 @@ do {
     correctGuess = true;
   }
 } while ( ! correctGuess )
-
 document.write('<h1>You guessed the number!</h1>');
 document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
+/*------could also write it this way----*/
+/*
+while ( guessCount < 10 ) {
+  guess = prompt('I am thinking of a number between 1 and 10. What is it?');
+  guessCount += 1;
+  if (parseInt(guess) === randomNumber) {
+    correctGuess = true;
+    break;
+  }
+}
+
+if ( correctGuess ) {
+    document.write('<h1>You guessed the number!</h1>');
+    document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
+} else {
+    document.write('<h1>Sorry. You did not guess the number</h1> ');
+}
+
+*/
